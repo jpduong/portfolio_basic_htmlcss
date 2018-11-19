8c605540ace03b020a95bba69c105d36aad2acab
James Phong Duong <james.duong93@gmail.com>
2018-11-19 18:41:59 +1100

    underline nav

--- script.js
+++ script.js
@@ -16,8 +16,3 @@ window.onscroll = () => {
   }
   prevScrollpos = currentScrollPos;
 }
-
-//carousel
-const mySiema = new Siema();
-document.querySelector('.prev').addEventListener('click', () => mySiema.prev());
-document.querySelector('.next').addEventListener('click', () => mySiema.next());
