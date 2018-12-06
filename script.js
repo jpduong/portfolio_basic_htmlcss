155962b8c5a55ffadb297af207fa49993026928f
James Phong Duong <james.duong93@gmail.com>
2018-12-06 17:32:48 +1100

    activating loader animation for live website

--- script.js
+++ script.js
@@ -1,11 +1,7 @@
-// window.addEventListener('load', function() {
-//     const loader = document.querySelector('.loader');
-//     loader.className += ' hidden';
-// });
 const navBar = document.getElementById("navbar"); 
 const toggle = document.getElementById("toggle");
 
-//navbar scroll 
+//navbar hide when scroll 
 let prevScrollpos = window.pageYOffset;
 window.onscroll = () => {
   let currentScrollPos = window.pageYOffset;
@@ -17,11 +13,11 @@ window.onscroll = () => {
   prevScrollpos = currentScrollPos;
 }
 
-//preloader
-// window.addEventListener('load', function() {
-//         const loader = document.querySelector('.loader');
-//         loader.className += ' hidden';
-//     });
+// preloader animation
+window.addEventListener('load', function() {
+        const loader = document.querySelector('.loader');
+        loader.className += ' hidden';
+    });
 
 // email function
 let email = document.getElementById("email").addEventListener("click", emailFunction);
@@ -35,4 +31,33 @@ function emailFunction() {
   document.body.removeChild(el);
   alert("Copied to clipboard: \n\njames.duong93@gmail.com");
 }
+
+//AOS initialization
+AOS.init();
+
+//smooth scrolling
+$(document).ready(function(){
+  // Add smooth scrolling to all links
+  $("a").on('click', function(event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      let hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
+});
 
