73b6697be3535a7c9114bb14611ab5dee2320f7d
James Phong Duong <james.duong93@gmail.com>
2018-12-09 19:56:08 +1100

    testing mobile view

--- script.js
+++ script.js
@@ -62,3 +62,10 @@ $(document).ready(function(){
 });
 
 
+//vh
+function calcVH() {
+  var vH = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+  document.getElementById("selector").setAttribute("style", "height:" + vH + "px;");
+}
+calcVH();
+window.addEventListener('onorientationchange', calcVH, true);
